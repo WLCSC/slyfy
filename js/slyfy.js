5141745718cd62afa56b04843f7d8370df4c09c3
samercier <merciers.merciers@gmail.com>
2013-12-30 22:17:38 -0600

    should be working

--- js/slyfy.js
+++ js/slyfy.js
@@ -1,12 +1,14 @@
 var fs = require('fs');
 var pix_regx = new RegExp("\.png|\.jpg|\.svg");
 var files;
-var directory = "C:\\Users\\Administrator\\Pictures";
+var directory = "/home/sam/Pictures";
 files = fs.readdirSync(directory);
 for( index = 0;index < files.length;index++) {
   file = files[index];
   if(JSON.stringify(file).match(pix_regx)) {
     console.log(file);
+    console.log("<div id='"+JSON.stringify(file).replace(/"/g, "")+"'> <img src='"+directory + "/"+JSON.stringify(file).replace(/"/g, "") +"' /></div>");
+    $("#slideshow_area").append("<div id=\""+JSON.stringify(file).replace(/"/g, "")+"\"> <img src='"+directory + "/"+JSON.stringify(file).replace(/"/g, "") +"' /></div>");
   }
 }
 /*while(true) {
