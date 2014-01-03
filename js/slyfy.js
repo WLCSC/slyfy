c74f5f7418d72c02d96306964f3186bdc4cc055a
Brad Thompson <brad@bradsotherbrain.com>
2014-01-03 13:30:58 -0500

    you can't append to something that doesn't exist yet.

--- js/slyfy.js
+++ js/slyfy.js
@@ -3,6 +3,8 @@ var pix_regx = new RegExp("\.png|\.jpg|\.svg");
 var files;
 var directory = "/home/sam/Pictures";
 files = fs.readdirSync(directory);
+
+$( function() {
 for( index = 0;index < files.length;index++) {
   file = files[index];
   if(JSON.stringify(file).match(pix_regx)) {
@@ -11,6 +13,8 @@ for( index = 0;index < files.length;index++) {
     $("#slideshow_area").append("<div id=\""+JSON.stringify(file).replace(/"/g, "")+"\"> <img src='"+directory + "/"+JSON.stringify(file).replace(/"/g, "") +"' /></div>");
   }
 }
+});
+
 /*while(true) {
 if(files == fs.readdirSync(directory)){
 console.log("Files are all the same.");
