04d5cc67a189d75f45bd95465a3786831cf44045
Brad Thompson <thompsonb@wl.k12.in.us>
2014-02-03 06:16:09 -1000

    animated stuff

--- js/slyfy.js
+++ js/slyfy.js
@@ -1,26 +1,58 @@
 var fs = require('fs');
-var pix_regx = new RegExp("\.png|\.jpg|\.svg");
+var pix_regx = new RegExp("\.[png|PNG|jpg|JPG|jpeg|JPEG]");
 var files;
-var directory = "/home/sam/Pictures";
+var directory = "pics";
 files = fs.readdirSync(directory);
 
 $( function() {
-for( index = 0;index < files.length;index++) {
-  file = files[index];
-  if(JSON.stringify(file).match(pix_regx)) {
-    console.log(file);
-    console.log("<div id='"+JSON.stringify(file).replace(/"/g, "")+"'> <img src='"+directory + "/"+JSON.stringify(file).replace(/"/g, "") +"' /></div>");
-    $("#slideshow_area").append("<div id=\""+JSON.stringify(file).replace(/"/g, "")+"\"> <img src='"+directory + "/"+JSON.stringify(file).replace(/"/g, "") +"' /></div>");
-  }
-}
+		console.log("howdy");
+    for( index = 0;index < files.length;index++) {
+        file = files[index];
+            console.log(file);
+        if(JSON.stringify(file).match(pix_regx)) {
+            console.log("<div id='"+JSON.stringify(file).replace(/"/g, "")+"'> <img src='"+directory + "/"+JSON.stringify(file).replace(/"/g, "") +"' /></div>");
+            $("#slideshow_area").append("<div id=\""+JSON.stringify(file).replace(/"/g, "")+"\"> <img src='"+directory + "/"+JSON.stringify(file).replace(/"/g, "") +"' /></div>");
+        }
+    }
 });
 
 /*while(true) {
-if(files == fs.readdirSync(directory)){
-console.log("Files are all the same.");
+  if(files == fs.readdirSync(directory)){
+  console.log("Files are all the same.");
+  }
+  else{
+  files = fs.readdirSync(directory);
+  }
+
+  }*/
+
+
+var count = files.length;
+var ctr = 0;
+
+function init() {
+    var offset = 0;
+    for(var i = 0; i < ctr; i++) {
+        offset += 720;
+    }
+    $('#slideshow_area').animate({top: '-='+offset}, 0);
 }
-else{
-files = fs.readdirSync(directory);
+function slideDown() {
+    $('#slideshow_area').animate({top: '-=720'}, 1000);
 }
 
-}*/
+function slideToTop() {
+    $('#slideshow_area').animate({top: '0'}, 0);
+}
+
+function timerTick() {
+    ctr++;
+    slideDown();
+    if(ctr >= count) {
+        slideToTop();
+        ctr = 0;
+    }
+}
+
+window.setInterval(function() { timerTick(); }, 11000);
+
